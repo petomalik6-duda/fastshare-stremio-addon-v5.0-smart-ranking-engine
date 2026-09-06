40d4c9b230ebb222aaa2c9ef8c9f92d6e6348b67
petomalik6-duda <petomalik6@gmail.com>
2026-09-06 11:19:31 +0200

    Bump addon version to 6.4.1

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const VERSION = '6.4.0';
+const VERSION = '6.4.1';
 const PORT = Number(process.env.PORT || 10000);
 const BASE_URL = String(process.env.BASE_URL || `http://localhost:${PORT}`).replace(/\/$/, '');
 const API = 'https://fastshare.cz/api/api_kodi.php';
